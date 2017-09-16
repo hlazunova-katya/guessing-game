c4a0d5cd4fe31d3fe4a7cd7764cf62119ba03c6f
hlazunova-katya <katrin_glazunova@mail.ru>
2017-09-16 18:03:41 +0300

    problem solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
 
+    constructor() {
+        this.numbers = [];
+        this.first = null;
+        this.last = null;
+        this.middle = null;
+    }
     setRange(min, max) {
 
+        if (min<max){
+            for (let i=min; i<=max; i++){
+                this.numbers.push(i);
+            }
+            this.first = 0;
+            this.last = this.numbers.length-1;
+        }
     }
-
     guess() {
-
+        this.middle = Math.round(this.first + (this.last - this.first) / 2);
+        return this.numbers[this.middle];
     }
 
     lower() {
-
+        this.last = this.middle;
     }
 
     greater() {
-
+        this.first = this.middle;
     }
 }
 
